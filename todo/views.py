ffcf65294dd15d7405b164b13cc592870c60caaf
Maks <maks98791@gmail.com>
2020-04-05 23:07:24 +0200

    Add working todo_add function

--- todo/views.py
+++ todo/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from django.utils import timezone
+from todo.models import Todo
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
 
 # Create your views here.
-from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
-    return render(request, 'todo/index.html')
+    todo_items = Todo.objects.all().order_by("-added_date")
+    return render(request, 'todo/index.html', {"todo_items": todo_items})
 
 @csrf_exempt
 def add_todo(request):
-    added_date = timezone.now()
+    current_date = timezone.now()
     content = request.POST["content"]
-    print(request.POST)
-    print(added_date)
-    print(content)
-    return render(request, 'todo/index.html')
+    obj = Todo.objects.create(added_date=current_date, text=content)
+    return HttpResponseRedirect("/")

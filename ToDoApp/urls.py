974f8efa8e33ddeedd045e598ac04c1b8dc7495a
Maks <maks98791@gmail.com>
2020-04-05 22:11:48 +0200

    Add base view and index view with basic elements (navbar, buttons form, inputs) and add_todo test function

--- ToDoApp/urls.py
+++ ToDoApp/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
+from todo import views
+
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('add_todo', views.add_todo),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
+
+def index(request):
+    return render(request, 'todo/index.html')
+
+@csrf_exempt
+def add_todo(request):
+    added_date = timezone.now()
+    content = request.POST["content"]
+    print(request.POST)
+    print(added_date)
+    print(content)
+    return render(request, 'todo/index.html')
